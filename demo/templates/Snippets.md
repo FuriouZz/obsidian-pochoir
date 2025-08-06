09195e4cca3e1642671fbbcb1298ad12fdb78a92
Christophe MASSOLIN <info@chrsmsln.com>
2025-08-06 02:28:44 +0200

    fix: sort snippet by match score

--- demo/templates/Snippets.md
+++ demo/templates/Snippets.md
@@ -1,3 +1,7 @@
 ```pochoir-snippet name="Date"
 {{date.today('YYYY-MM-DD')}}
 ```
+
+```pochoir-snippet
+{{date.today('YYYY-MM-DD')}}
+```
--- src/extensions/snippet-extension.ts
+++ src/extensions/snippet-extension.ts
@@ -73,7 +73,8 @@ export default function (): Extension {
                                     score,
                                 },
                             };
-                        });
+                        })
+                        .sort((a, b) => b.match.score - a.match.score);
                 },
             });
         },
